7f4cede011eb333b24f46f7ec067bd60625ea3cb
Lin Yiting <linytsysu@163.com>
2016-06-01 13:16:58 +0800

    EnableWebSecurity

--- src/main/java/web/config/WebSecurityConfig.java
+++ src/main/java/web/config/WebSecurityConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -17,7 +17,7 @@ import web.security.CustomUserDetailsService;;
 
 @SuppressWarnings("deprecation")
 @Configuration
-@EnableWebMvcSecurity
+@EnableWebSecurity
 @ComponentScan(basePackageClasses = CustomUserDetailsService.class)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
